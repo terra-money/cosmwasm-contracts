69aa0087edaae259e0bf709a7b1585ca58bae33b
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-05-20 10:25:51 +0200

    Add all other custom queries

--- packages/bindings/src/query.rs
+++ packages/bindings/src/query.rs
@@ -1,27 +1,15 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::{Coin, Decimal, Querier, QueryRequest, StdResult};
+use cosmwasm_std::{Coin, Decimal, Querier, QueryRequest, StdResult, Uint128};
 
 /// TerraQuery is an override of QueryRequest::Custom to access Terra-specific modules
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum TerraQuery {
     Swap(SwapQuery),
-    // TODO: add for treasury and oracle
-}
-
-/// This contains all queries that can be made to the swap module
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
-pub enum SwapQuery {
-    // ExchangeRate will return the rate between just this pair.
-    ExchangeRate { offer: String, ask: String },
-    // ExchangeRates will return the exchange rate between offer denom and all supported asks
-    ExchangeRates { offer: String },
-    // Delegations will return all delegations by the delegator,
-    // or just those to the given validator (if set)
-    Simulate { offer: Coin, ask: String },
+    Oracle(OracleQuery),
+    Treasury(TreasuryQuery),
 }
 
 // This is a simpler way to making queries
@@ -31,6 +19,15 @@ impl Into<QueryRequest<TerraQuery>> for TerraQuery {
     }
 }
 
+/// This contains all queries that can be made to the swap module
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum SwapQuery {
+    // Delegations will return all delegations by the delegator,
+    // or just those to the given validator (if set)
+    Simulate { offer: Coin, ask: String },
+}
+
 // This is a simpler way to making queries
 impl Into<QueryRequest<TerraQuery>> for SwapQuery {
     fn into(self) -> QueryRequest<TerraQuery> {
@@ -38,24 +35,96 @@ impl Into<QueryRequest<TerraQuery>> for SwapQuery {
     }
 }
 
-/// ExchangeRateResponse is data format returned from SwapRequest::ExchangeRate query
+/// SimulateSwapResponse is data format returned from SwapRequest::Simulate query
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct SimulateSwapResponse {
+    pub receive: Coin,
+}
+
+/// This contains all queries that can be made to the oracle module
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum OracleQuery {
+    // ExchangeRate will return the rate between just this pair.
+    ExchangeRate { offer: String, ask: String },
+    // ExchangeRates will return the exchange rate between offer denom and all supported asks
+    ExchangeRates { offer: String },
+    // Return the tobin tax charged on exchanges with this token
+    // (TODO: define if this applies to the offer or the ask?)
+    TobinTax { denom: String},
+}
+
+/// ExchangeRateResponse is data format returned from OracleRequest::ExchangeRate query
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct ExchangeRateResponse {
     pub ask: String,
     pub rate: Decimal,
 }
 
-/// ExchangeRatesResponse is data format returned from SwapRequest::ExchangeRates query
+/// ExchangeRatesResponse is data format returned from OracleRequest::ExchangeRates query
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct ExchangeRatesResponse {
     pub rates: Vec<ExchangeRateResponse>,
 }
 
-/// SimulateSwapResponse is data format returned from SwapRequest::Simulate query
+/// TobinTaxResponse is data format returned from OracleRequest::TobinTax query
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct SimulateSwapResponse {
-    pub receive: Coin,
+pub struct TobinTaxResponse {
+    pub tax: Decimal,
 }
+
+// This is a simpler way to making queries
+impl Into<QueryRequest<TerraQuery>> for OracleQuery {
+    fn into(self) -> QueryRequest<TerraQuery> {
+        QueryRequest::Custom(TerraQuery::Oracle(self))
+    }
+}
+
+/// This contains all queries that can be made to the treasury module
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum TreasuryQuery {
+    TaxRate {},
+    TaxProceeds{},
+    // TODO: review
+    TaxCap { denom: String },
+    RewardWeight {},
+    SeigniorageProceeds {},
+}
+
+/// TaxRateResponse is data format returned from TreasuryRequest::TaxRate query
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct TaxRateResponse {
+    pub tax: Decimal,
+}
+
+/// TaxProceedsResponse is data format returned from TreasuryRequest::TaxProceeds query
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct TaxProceedsResponse {
+    pub proceeds: Vec<Coin>,
+}
+
+/// TaxCapResponse is data format returned from TreasuryRequest::TaxCap query
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct TaxCapResponse {
+    // TODO: verify
+    pub cap: Uint128,
+}
+
+/// RewardsWeightResponse is data format returned from TreasuryRequest::RewardsWeight query
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct RewardsWeightResponse {
+    pub weight: Decimal,
+}
+
+/// SeigniorageProceedsResponse is data format returned from TreasuryRequest::SeigniorageProceeds query
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct SeigniorageProceedsResponse {
+    // TODO: verify what this is
+    pub size: Uint128,
+}
+
+/***** TODO move to traits *********/
 
 /// This is a helper wrapper to easily use our custom queries
 pub struct TerraQuerier<'a, Q: Querier> {
@@ -68,7 +137,7 @@ impl<'a, Q: Querier> TerraQuerier<'a, Q> {
     }
 
     pub fn query_exchange_rate<T: Into<String>>(&self, offer: T, ask: T) -> StdResult<Decimal> {
-        let request = SwapQuery::ExchangeRate {
+        let request = OracleQuery::ExchangeRate {
             offer: offer.into(),
             ask: ask.into(),
         };
@@ -80,7 +149,7 @@ impl<'a, Q: Querier> TerraQuerier<'a, Q> {
         &self,
         offer: T,
     ) -> StdResult<Vec<ExchangeRateResponse>> {
-        let request = SwapQuery::ExchangeRates {
+        let request = OracleQuery::ExchangeRates {
             offer: offer.into(),
         };
         let res: ExchangeRatesResponse = self.querier.custom_query(&request.into())?;
