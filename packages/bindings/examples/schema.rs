f9a3427002b837430d20bd6b456f381b11134522
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-05-20 14:30:51 +0200

    Cleanup on generating schemas

--- packages/bindings/examples/schema.rs
+++ packages/bindings/examples/schema.rs
@@ -1,7 +1,7 @@
 use std::env::current_dir;
 use std::fs::create_dir_all;
 
-use cosmwasm_schema::{export_schema, schema_for};
+use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 use terra_bindings::{
     ExchangeRateResponse, ExchangeRatesResponse, RewardsWeightResponse,
     SeigniorageProceedsResponse, SimulateSwapResponse, TaxCapResponse, TaxProceedsResponse,
@@ -12,6 +12,7 @@ fn main() {
     let mut out_dir = current_dir().unwrap();
     out_dir.push("schema");
     create_dir_all(&out_dir).unwrap();
+    remove_schemas(&out_dir).unwrap();
 
     export_schema(&schema_for!(TerraMsg), &out_dir);
     export_schema(&schema_for!(TerraQuery), &out_dir);
