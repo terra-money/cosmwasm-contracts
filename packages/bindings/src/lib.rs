acb8f76ce1624a7c7bf9be7a8c1accd5da4b20bf
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-05-20 11:38:18 +0200

    Add requires_terra

--- packages/bindings/src/lib.rs
+++ packages/bindings/src/lib.rs
@@ -9,3 +9,8 @@ pub use query::{
     SeigniorageProceedsResponse, SimulateSwapResponse, SwapQuery, TaxCapResponse,
     TaxProceedsResponse, TaxRateResponse, TerraQuery, TobinTaxResponse, TreasuryQuery,
 };
+
+// This export is added to all contracts that import this package, signifying that they require
+// "terra" support on the chain they run on.
+#[no_mangle]
+extern "C" fn requires_terra() -> () {}
