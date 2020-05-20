574e29e6d7b77bca383cbb1c53c378757df33676
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-05-20 11:57:04 +0200

    Fix up integration tests

--- contracts/maker/tests/integration.rs
+++ contracts/maker/tests/integration.rs
@@ -17,9 +17,11 @@
 //!      });
 //! 4. Anywhere you see query(&deps, ...) you must replace it with query(&mut deps, ...)
 
-use cosmwasm_std::testing::mock_env;
-use cosmwasm_std::{coins, from_binary, InitResponse};
-use cosmwasm_vm::testing::{init, mock_instance, query};
+use cosmwasm_std::{coins, from_binary, Coin, InitResponse};
+use cosmwasm_vm::testing::{
+    init, mock_dependencies, mock_env, query, MockApi, MockQuerier, MockStorage,
+};
+use cosmwasm_vm::Instance;
 
 use maker::msg::{ConfigResponse, InitMsg, QueryMsg};
 use terra_bindings::TerraMsg;
@@ -28,6 +30,19 @@ use terra_bindings::TerraMsg;
 static WASM: &[u8] = include_bytes!("../target/wasm32-unknown-unknown/release/maker.wasm");
 // You can uncomment this line instead to test productionified build from cosmwasm-opt
 // static WASM: &[u8] = include_bytes!("../contract.wasm");
+
+const DEFAULT_GAS_LIMIT: u64 = 500_000;
+
+pub fn mock_instance(
+    wasm: &[u8],
+    contract_balance: &[Coin],
+) -> Instance<MockStorage, MockApi, MockQuerier> {
+    // TODO: check_wasm is not exported from cosmwasm_vm
+    // let terra_features = features_from_csv("staking,terra");
+    // check_wasm(wasm, &terra_features).unwrap();
+    let deps = mock_dependencies(20, contract_balance);
+    Instance::from_code(wasm, deps, DEFAULT_GAS_LIMIT).unwrap()
+}
 
 #[test]
 fn proper_initialization() {
