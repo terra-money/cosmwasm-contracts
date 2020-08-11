ceccebf8b8f5a0ec717c4bea8046ce811e92c0b8
yys <sw.yunsuk@gmail.com>
2020-08-11 17:53:46 +0900

    update schema

--- contracts/mask/examples/schema.rs
+++ contracts/mask/examples/schema.rs
@@ -4,8 +4,8 @@ use std::fs::create_dir_all;
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 use cosmwasm_std::HandleResponse;
 
-use reflect::msg::{CustomMsg, HandleMsg, InitMsg, OwnerResponse, QueryMsg};
-use reflect::state::State;
+use cw_mask::msg::{CustomMsgWrapper, HandleMsg, InitMsg, OwnerResponse, QueryMsg};
+use cw_mask::state::State;
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -13,10 +13,10 @@ fn main() {
     create_dir_all(&out_dir).unwrap();
     remove_schemas(&out_dir).unwrap();
 
-    export_schema(&schema_for!(CustomMsg), &out_dir);
+    export_schema(&schema_for!(CustomMsgWrapper), &out_dir);
     export_schema(&schema_for!(InitMsg), &out_dir);
     export_schema(&schema_for!(HandleMsg), &out_dir);
-    export_schema(&schema_for!(HandleResponse<CustomMsg>), &out_dir);
+    export_schema(&schema_for!(HandleResponse<CustomMsgWrapper>), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
     export_schema(&schema_for!(State), &out_dir);
     export_schema(&schema_for!(OwnerResponse), &out_dir);
